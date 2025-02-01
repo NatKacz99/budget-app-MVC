f7aa4513c953a44af7e6ac8f152fa5d6a4b517cf
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-02-01 10:55:17 +0100

    global template variables

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -6,11 +6,14 @@ namespace Framework;
 
 class TemplateEngine
 {
+  private array $globalTemplateData = [];
+
   public function __construct(private string $basePath) {}
 
   public function render(string $template, array $data = [])
   {
     extract($data, EXTR_SKIP);
+    extract($this->globalTemplateData, EXTR_SKIP);
 
     ob_start();
 
@@ -21,5 +24,10 @@ class TemplateEngine
     ob_end_clean();
 
     return $output;
+  }
+
+  public function addGlobal(string $key, mixed $value)
+  {
+    $this->globalTemplateData[$key] = $value;
   }
 }
