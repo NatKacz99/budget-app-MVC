dbaeebdb6cbb80768e7f7e11147d8f4f4b98375b
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-02-11 19:18:16 +0100

    paths correct

--- src/App/functions.php
+++ src/App/functions.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 function dd(mixed $value)
 {
-  echo "<pre>";
-  var_dump($value);
-  echo "</pre>";
-  die();
+  if (getenv('APP_ENV') !== 'production') {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+  }
 }
 
 function e($value): string
 {
-  return htmlspecialchars((string) $value);
+  return htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8', false);
 }
