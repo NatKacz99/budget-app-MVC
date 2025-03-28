14c31f2e8dd9f4808d81eeb127b608370d70e615
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2025-03-28 17:46:35 +0100

    extracting route parameter values

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -41,11 +41,17 @@ class Router
 
     foreach ($this->routes as $route) {
       if (
-        !preg_match("#^{$route['regexPath']}$#", $path) ||
+        !preg_match("#^{$route['regexPath']}$#", $path, $paramValues) ||
         $route['method'] !== $method
       ) {
         continue;
       }
+
+      array_shift($paramValues);
+      preg_match_all('#{([^/]+)}#', $route['path'], $paramKeys);
+      $paramKeys = $paramKeys[1];
+
+      $params = array_combine($paramKeys, $paramValues);
 
       [$class, $function] = $route['controller'];
 
@@ -53,7 +59,7 @@ class Router
         $container->resolve($class) :
         new $class;
 
-      $action = fn() => $controllerInstance->{$function}();
+      $action = fn() => $controllerInstance->{$function}($params);
 
       $allMiddleware = [...$route['middlewares'], ...$this->middlewares];
 
